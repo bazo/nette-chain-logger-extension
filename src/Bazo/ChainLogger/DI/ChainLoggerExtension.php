5dcc0a73c1f8e916775ce5ff476801100d55d42c
Martin Bazik <martin@bazo.sk>
2013-10-30 22:16:23 +0100

    Initial commit

---
+++ src/Bazo/ChainLogger/DI/ChainLoggerExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Bazo\ChainLogger\DI;
+
+/**
+ * Description of ChainLoggerExtension
+ *
+ * @author Martin Bažík <martin@bazo.sk>
+ */
+class ChainLoggerExtension extends \Nette\DI\CompilerExtension
+{
+	/** @var Logger */
+	private $logger;
+	
+	private $defaults = [
+		'loggers' => [],
+	];
+	
+	public function loadConfiguration()
+	{
+		$containerBuilder = $this->getContainerBuilder();
+		$config = $this->getConfig($this->defaults);
+
+		$logger = $containerBuilder->addDefinition($this->prefix('logger'))
+						->setClass('ChainLogger\Logger');
+		
+		foreach ($config['loggers'] as $loggerName => $implementation) {
+			$this->compiler->parseServices($containerBuilder, [
+				'services' => [
+					$this->prefix($loggerName) => $implementation,
+				],
+			]);
+
+			$logger->addSetup('addLogger', [$this->prefix('@' . $loggerName)]);
+		}
+		
+		$containerBuilder instanceof \Nette\DI\ContainerBuilder;
+		foreach(array_keys($containerBuilder->findByTag('logger')) as $loggerName) {
+			$logger->addSetup('addLogger', ['@' . $loggerName]);
+		}
+	}
+
+	public function afterCompile(\Nette\PhpGenerator\ClassType $class)
+	{
+		$initialize = $class->methods['initialize'];
+
+		$initialize->addBody('\Nette\Diagnostics\Debugger::$logger = $this->getService(?);', [$this->prefix('logger')]);
+	}
+}
+
---
+++ src/Bazo/ChainLogger/Logger.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bazo\ChainLogger\DI;
+
+use \Nette\Diagnostics\Logger as NetteLogger;
+
+/**
+ * Description of Logger
+ *
+ * @author Martin Bažík <martin@bazo.sk>
+ */
+class Logger extends NetteLogger
+{
+	/** @var NetteLogger[] */
+	private $loggers = [];
+
+	public function addLogger(NetteLogger $logger)
+	{
+		$this->loggers[spl_object_hash($logger)] = $logger;
+	}
+
+	public function log($message, $priority = self::INFO)
+	{
+		foreach($this->loggers as $logger)
+		{
+			$logger->log($message, $priority);
+		}
+	}
+
+	public static function register()
+	{
+		\Nette\Diagnostics\Debugger::$logger = new static();
+	}
+}
+
